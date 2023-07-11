0182445ec52a35e27bb8ea1484e1fa55f4c2ef2a
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-07-11 21:22:55 +0700

    add view binding scan activity

--- app/src/main/java/com/example/besideme/ScanActivity.kt
+++ app/src/main/java/com/example/besideme/ScanActivity.kt
@@ -2,10 +2,14 @@ package com.example.besideme
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.besideme.databinding.ActivityHomeBinding
+import com.example.besideme.databinding.ActivityScanBinding
 
 class ScanActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityScanBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityScanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
